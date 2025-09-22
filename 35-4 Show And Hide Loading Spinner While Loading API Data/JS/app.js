dce397536597c25b403855b355adafd1bfa744e0
shihab20105 <shihabur10@gmail.com>
2025-09-22 12:38:18 +0100

    Examined Show and hide loading spinner while loading API data

---
+++ 35-4 Show And Hide Loading Spinner While Loading API Data/JS/app.js
@@ -0,0 +1,62 @@
+/*                      35-4 Show and hide loading spinner while loading API data */
+
+const loadPhones = async (searchText) => {
+    const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`
+    const res = await fetch(url);
+    const data = await res.json();
+    displayPhones(data.data);
+};
+
+const displayPhones = phones => {
+    // console.log(phones);
+    const phonesContainer = document.getElementById('phones-container');
+    phonesContainer.textContent = '';
+    // Display 20 Phones only
+    phones = phones.slice(0,10);
+    // Display All Phones
+    const noPhone = document.getElementById('no-found-message');
+    if(phones.length === 0){
+        noPhone.classList.remove('d-none');
+    }
+    else{
+        noPhone.classList.add('d-none');
+    }
+    // Display No Phones Found 
+    phones.forEach(phone => {
+        const phoneDiv = document.createElement('div');
+        phoneDiv.classList.add('col');
+        phoneDiv.innerHTML = `
+        <div class="card p-4">
+            <img src="${phone.image}" class="card-img-top" alt="...">
+            <div class="card-body">
+            <h5 class="card-title">${phone.phone_name}</h5>
+            <p class="card-text">This is a longer card with supporting text below as a natural lead-in
+            to additional content. This content is a little bit longer.</p>
+            </div>
+        </div>
+        `;
+        phonesContainer.appendChild(phoneDiv);
+    });
+    // Stop Spinner/Loader
+    toggleSpinner(false);
+}
+// Handle Search Button Click
+document.getElementById('btn-search').addEventListener('click', function(){
+    // Start Spinner/Loader
+    toggleSpinner(true);
+    const searchField = document.getElementById('search-field');
+    const searchText = searchField.value;
+    loadPhones(searchText);
+})
+
+const toggleSpinner = isLoading => {
+    const loaderSection = document.getElementById('loader');
+    if(isLoading){
+        loaderSection.classList.remove('d-none');
+    }
+    else{
+        loaderSection.classList.add('d-none');
+    }
+}
+
+// loadPhones();
